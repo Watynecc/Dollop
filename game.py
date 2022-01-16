4b5569bf948536f8cab1e1f22fc0ab082a96274b
Watynecc <64525273+Watynecc@users.noreply.github.com>
2022-01-16 09:04:10 +0100

    Updated game.py

--- game.py
+++ game.py
@@ -1,2 +1,17 @@
 import pygame
-prin
+pygame.init()
+
+screen = pygame.display.set_mode([500, 400])
+
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    screen.fill((255, 255, 255))
+
+    pygame.draw.circle(screen, (0, 255, 255), (250, 200), 75)
+    pygame.display.flip()
+
+pygame.quit()
